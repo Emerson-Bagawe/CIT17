1e2407ae240e9c7eeeed155400ce68081f33c7d1
unknown <CIT173E-20@M303.UC-BCF.EDU.PH>
2023-10-05 17:49:38 +0800

    added Gitcommands

---
+++ GitCommands.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>PHP Project</title>
+
+    <head>
+
+    <body>
+        <?php
+        $git = "git init<br>";
+        print($git);
+        $git = "git add README.MD<br>";
+        print($git);
+        $git = "git commit -m 'comments'<br>";
+        print($git);
+        $git = "git branch -M main<br>";
+        print($git);
+        $git = "git remote add origin 'https link of github'<br>";
+        print($git);
+        $git = "git push -u origin main<br>";
+        print($git);
+        $git = "*if errors occured*<br>";
+        print($git);
+        $git = "git pull<br>";
+        print($git);
+        $git = "git fetch origin main:tmp<br>";
+        print($git);
+        $git = "git rebase tmp<br>";
+        print($git);
+        $git = "git push<br>";
+        print($git);
+        $git = "*if di nag push*<br>";
+        print($git);
+        $git = "git push -set--upstream origin main<br>";
+        print($git);
+        $git = "git clone 'https link of github'<br>";
+        print($git);
+        $git = "git pull<br>";
+        print($git);
+        ?>
+    </body>
+
+</html>
