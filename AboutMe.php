81f9bc2106a0253a9b2f622c731efae27343b6d4
Emerson-Bagawe <eob2286@students.uc-bcf.edu.ph>
2023-10-07 14:46:38 +0800

    sorting gitcommands

--- AboutMe.php
+++ AboutMe.php
@@ -1,23 +1,24 @@
 <!DOCTYPE html>
+<link rel="stylesheet" href="">
 <html>
 
 <head>
-    <title>PHP Project</title>
+	<title>PHP Project</title>
 
-    <head>
+	<head>
 
-    <body>
-        <?php
-        $name = "Bagawe, Emerson O.";
-        $hobbie = "playing games, working out";
-        $interest = "";
+	<body>
+		<?php
+		$name = "Bagawe, Emerson O.";
+		$hobbie = "playing games, working out";
+		$interest = "";
 
-        $aboutMe = 'My $variable will not print!<br>';
-        $myName = "My name is $name <br>";
-        print($myName);
-        $myHobby = "My hobbies are $hobbie <br>";
-        print($myHobby);
-        ?>
-    </body>
+		$aboutMe = 'My $variable will not print!<br>';
+		$myName = "My name is $name <br>";
+		print($myName);
+		$myHobby = "My hobbies are $hobbie <br>";
+		print($myHobby);
+		?>
+	</body>
 
 </html>
